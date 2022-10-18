5e2e72922132300ecad33b71e2d6b9e9ba153ee3
wasim86 <mohdwasim1996@gmail.com>
2022-10-18 10:25:21 +0530

    get-api assgnmnt completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,43 +1,88 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
-
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
+// 1
+router.get("/movies", function (req, res) {
+    let arr= ["captain America" ,"hulk", "infinity war","3 Idiots"];
+ res.send(arr)
 });
 
-router.get('/students', function (req, res){
-    console.log("The path params in the request are : ", req.params)
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+// 2
+router.get("/movies/:indexNumber", function (req, res){
+    
+
+    let arr= ["captain America" ,"hulk", "infinity war","3 Idiots"];
+    let index1 = req.params.indexNumber
+    if (index1 > arr.length || index1==arr.length){
+        res.send("enter a valid index");
+    }
+    else{
+        
+    res.send(arr[index1])
+        
+    }
+  
 })
 
 
-// Example 1 for path params
-router.get('/students/:studentName', function(req, res){
-    // ':' denotes that the following part of route is a variable
-    // The value of this variable is what we are sending in the request url after /students
-    // This value is set in the form of an object inside req.params
-    // The object contain key value pairs
-    // key is the variable in the route
-    // value is whatever dynamic value sent in the request url
-    let myParams = req.params
+// 3
+router.get('/films', function(req, res){
+    let arr1=
+    [{
+        id:1,
+        name:"infinity war"}
+        ,
+        {
+            id:2,
+            name:"3Idiots"
+        },
+        {
+            id:3,
+            name:"Dhoom3"
+        },
+        { id:4,
+            name:"war"
+        }]
 
-    // params attribute is fixed in a request object
-    // params contains the path parameters object
-    console.log("The path params in the request are : ", myParams)
-    res.send('The full name is ' + myParams.studentName )
+
+
+
+
+     res.send(arr1)
 })
 
-// Example 2 for path params
-router.get('/student-details/:name', function(req, res){
-    let requestParams = req.params
-    console.log("This is the request ", requestParams)
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    res.send('Dummy response')
+
+// 4
+router.get("/films/:filmID", function (req, res){
+
+    let arr1=
+    [{
+        id:0,
+        name:"infinity war"}
+        ,
+        {
+            id:1,
+            name:"3Idiots"
+        },
+        {
+            id:2,
+            name:"Dhoom3"
+        },
+        { id:3,
+            name:"war"
+        }]
+
+        let iD= req.params.filmID
+
+        if(iD > arr1.length || iD== arr1.length){
+           
+            res.send("no movie exist here with this present ID")
+
+        }
+        else{
+           
+            res.send(arr1[iD])
+        }
 })
 
 module.exports = router;
