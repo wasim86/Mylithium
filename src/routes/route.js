3d321c1cb012965c3290b18800c058266593686f
wasim86 <mohdwasim1996@gmail.com>
2022-10-20 10:13:28 +0530

    post api assgnmnt done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,96 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+//1
+    router.post("/sol1", function (req, res) {
+        let input =[1,2,3,5,6,7]
+        let N = input.length+1
+        let first = input[0]
+        let last =input[input.length-1]
+        var sum=N*(first+last);
+        sum=sum/2;
+        var output= 0;
+        
+        for (var i = 0; i < input.length; i++) {
+          output=output+input[i];
+        }
+        let missingNumber=sum-output
+        console.log(missingNumber)
+        res.send({data:missingNumber})
+        
+    })
+
+   
+    
+// 02
+    router.get("/sol2", function(req, res){
+        let input =[22,23,24,25,26,28]
+        let N = input.length+1
+        let first = input[0]
+        let last =input[input.length-1]
+        var sum=N*(first+last);
+        sum=sum/2;
+        var output= 0;
+        
+        for (var i = 0; i < input.length; i++) {
+          output=output+input[i];
+        }
+        let missingNumber=sum-output
+        console.log(missingNumber)
+        res.send({data:missingNumber})
+        
+        
+    
+    })
+    
+    // 3
+    const playerObject=[ {
+
+        name: "manish",
+        dob: "1/1/1995",
+        gender: "male",
+        city: "jalandhar",
+        sports: [
+        "swimming"
+        ]
+        },
+        { name: "ayush",
+          dob: "1/1/2000",
+    
+        }]
+    
+    // soln
+    router.post("/players", function(req, res) {
+        let newData=req.body
+       
+        
+        for (let i = 0; i < playerObject.length; i++) {
+            const element = playerObject[i];
+            if(element.name==newData.name){
+                console.log("person exist")
+                res.send("person alrdy existed")
+            }
+            
+            
+        }
+        playerObject.push(newData)
+    
+    
+        
+    console.log(playerObject)
+    res.send({playerObject , status: true})
+    
+    })
+    
+
+   
 
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
+    
 
 
 
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
 
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
 
 module.exports = router;
